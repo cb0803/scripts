3dc54d8c48d065e224b55c7bc4b45c8e8e89ed00
Ciaran Baker <55151549+cb0803@users.noreply.github.com>
2020-10-29 10:29:22 +0000

    Update creation.sh

--- creation.sh
+++ creation.sh
@@ -16,6 +16,3 @@ sshpass -p "" scp root@10.106.0.2:/root/backup.sh /root
 chmod +x /root/backup.sh
 
 { crontab -l -u root; echo '0 */6 * * * /root/backup.sh'; } | crontab -u root -
-
-echo -e "\n" | \add-apt-repository ppa:certbot/certbot
-apt-get install python-certbot-apache

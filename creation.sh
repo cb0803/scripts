5529fe16e9db48bfef21b9be862cc3da9e1d4635
Ciaran Baker <55151549+cb0803@users.noreply.github.com>
2020-10-29 10:33:49 +0000

    Update creation.sh

--- creation.sh
+++ creation.sh
@@ -7,7 +7,7 @@ apt-get -y install sshpass apache2 php mysql-server zip unzip
 
 mysql -e "create database client;"
 mysql -e "create user 'client'@'localhost' identified with mysql_native_password by '$mysql';"
-mysql -e "grant all privileges on client to 'client'@'localhost';"
+mysql -e "grant all privileges on client.* to 'client'@'localhost';"
 mysql -e "flush privileges;"
 
 ssh-keyscan -H 10.106.0.2 >> ~/.ssh/known_hosts
